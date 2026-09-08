91be2184689d046b0abbbc61b654d13a234fe22c
John Horan <jhoran@zendesk.com>
2026-09-08 10:47:32 +0100

    back to aioboto3

--- astronomer_starship/_af3/starship_compatability.py
+++ astronomer_starship/_af3/starship_compatability.py
@@ -12,6 +12,7 @@ import aiobotocore
 import aiobotocore.client
 import aiobotocore.session
 import boto3
+import aioboto3
 from airflow.configuration import conf
 from fastapi import Request
 
@@ -1419,19 +1420,18 @@ class StarshipAirflow33(StarshipAirflow32):
         return await asyncio.to_thread(self._sync_set_task_log, body=body, conn_id=conn_id, path=path, **kwargs)
 
     @staticmethod
-    def create_async_session_from_sync(sync_session: boto3.Session) -> aiobotocore.session.AioSession:
+    def create_async_session_from_sync(sync_session: boto3.Session) -> aioboto3.Session:
         # 1. Fetch credentials from the sync session
         credentials = sync_session.get_credentials()
 
         # 2. Extract specific auth elements (handles temporary/session tokens too)
         frozen_creds = credentials.get_frozen_credentials()
 
-        session = aiobotocore.session.get_session()
-        session.set_credentials(
-            access_key=frozen_creds.access_key,
-            secret_key=frozen_creds.secret_key,
-            token=frozen_creds.token,
-            account_id=frozen_creds.account_id,
+        session = aioboto3.Session(
+            aws_access_key_id=frozen_creds.access_key,
+            aws_secret_access_key=frozen_creds.secret_key,
+            aws_session_token=frozen_creds.token,
+            aws_account_id=frozen_creds.account_id,
         )
         return session
 
@@ -1451,7 +1451,7 @@ class StarshipAirflow33(StarshipAirflow32):
             temp_file.flush()
             temp_file.seek(0)
 
-            async with session.create_client("s3") as s3:
+            async with session.client("s3") as s3:
                 await s3.upload_fileobj(Fileobj=temp_file, Bucket=bucket, Key=blob_s3_key)
 
     def _sync_set_task_log(self, body: bytes, dag_id: str, run_id: str, conn_id: str, path: str, **kwargs):
